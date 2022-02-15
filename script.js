35e6692c7e45b0a8bf936ee176a82d6bcf82e371
Harmeet Matharoo <hmjatt@gmail.com>
2022-02-14 23:30:02 -0700

    changed theme

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //script for Library App
+
+const getStarted = document.getElementById("get-started-button");
+const welcomePage = document.getElementsByClassName("welcome-page");
+const page2 = document.getElementsByClassName("page-2");
+
+
+getStarted.addEventListener("click", function con() {
+    console.log("hi");
+    // page2.style.display = "block";
+    console.log(welcomePage);
+});
+
+
