3a5aebdfd0eee3ae01e225e0461a2089c371e6c4
Harmeet Matharoo <hmjatt@gmail.com>
2022-02-21 02:09:47 -0700

    grid is responsive

--- script.js
+++ script.js
@@ -1,62 +1,8 @@
-// //script for Library App
-
-// // const getStarted = document.getElementById("get-started-button");
-// // const welcomePage = document.getElementsByClassName("welcome-page");
-// // const page2 = document.getElementsByClassName("page-2");
-
-
-// // getStarted.addEventListener("click", function con() {
-// //     console.log("hi");
-// //     // page2.style.display = "block";
-// //     console.log(welcomePage);
-// // });
-
-
-
-
-// // Wait for the page to load first
-// window.onload = function() {
-
-//     //Get a reference to the link on the page
-//     // with an id of "mylink"
-//     var a = document.getElementById("mylink");
-
-//     //Set code to run when the link is clicked
-//     // by assigning a function to "onclick"
-//     a.onclick = function() {
-
-//         // Your code here...
-
-//         //If you don't want the link to actually 
-//         // redirect the browser to another page,
-//         // "google.com" in our example here, then
-//         // return false at the end of this block.
-//         // Note that this also prevents event bubbling,
-//         // which is probably what we want here, but won't 
-//         // always be the case.
-//         return false;
-//     }
-// }
-
-
-// load page 2 on click of button 
-
-// const getStartedButton = document.getElementById("get-started-button");
-
-
-// getStartedButton.addEventListener("click", function() {
-//   window.location.href='/page-2.html';
-// });
-
-
-// onclick="window.location.href='/page-2.html';
-
-
+//script for Library App
 
 
 
 // code to search for content
-
 
 function search() {
     let input = document.getElementById("search-item").value
@@ -210,14 +156,4 @@ Array.from(addNotesBtn).forEach((addBtn) => {
 
   });
 });
-
-  // Array.from(notesPara).forEach((para) => {
-  //   para.addEventListener("click", function() {
-  //     let text = btn.nextSibling.nextSibling;
-      
-  //     text.classList.toggle("hide");
-  //     text.classList.toggle("show");
-
-  //   });
-// });
 
